d1c0f6f46306d1a9fa5852fe3055c980a6690e0c
DEBADRIBASAK <32904247+DEBADRIBASAK@users.noreply.github.com>
2018-08-29 22:36:51 +0530

    Update PartitionBasedApriori.cpp

--- AprioriAlgorithm/PartitionBasedApriori.cpp
+++ AprioriAlgorithm/PartitionBasedApriori.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <vector>
 #include <set>
 #include <sstream>
 #include <string>
@@ -9,183 +10,179 @@
 #define TRAN_NUM 51
 #define CHUNK 3
 #define MIN_SUPPORT_PART (int)ceil(((double)MIN_SUPPORT/(double)TRAN_NUM)*cnt)
+#define vector_ITR vector<int>::iterator
+#define VEC_ITR vector<vector<int> >::iterator
 #define SET_ITR set<int>::iterator
-#define MAP_ITR map<set<int>,int>::iterator
-#define MAP_ITR2  map<int,set<int> >::iterator
+#define MAP_ITR map<vector<int>,int>::iterator
+#define MAP_ITR2  map<int,vector<int> >::iterator
 using namespace std;
 
-map<int,set<int> > tran;
-map<set<int>,int> final_set;
+map<int,vector<int> > tran;
+map<vector<int>,int> final_vector;
 ifstream in;
 bool f;
 int cnt;
-void print_set(ostream& out,set<int> s);
+void print_vector(ostream& out,vector<int> s);
+
+
+void init()
+{
+    in.open("Transactions1.txt");
+}
 
 /**
 the following function is used for reading the initial data from the transaction database and prepare L1 and C1
 **/
-void init()
+
+
+vector <vector<int> > read_vector(ofstream &out)
 {
-    in.open("Transactions.txt");
-}
-map<set<int>,int> read_set(ofstream &out)
-{
-   // exit(0);
     string s="";
     int t,tr;
     cnt = 0;
-    map<set<int>,int> rtn,rtn1;
-    set<int> v;//,it;
+    set<int> v;
     while(!in.eof()&&cnt<CHUNK)
     {
         getline(in,s);
-        //cout<<"s = "<<s<<endl;
         stringstream str(s);
         str>>tr;
         while(str>>t)
         {
             v.insert(t);
-            tran[tr].insert(t);
-            rtn[v]++;
-            v.clear();
+            tran[tr].push_back(t);
         }
         cnt++;
-       // cout<<cnt<<endl;
     }
     if(in.eof())
     {
         f = false;
         in.close();
     }
-    out<<"C1 :"<<endl;
-    for(MAP_ITR itr = rtn.begin();itr!=rtn.end();itr++)
+    vector<vector<int> > rtn(v.size());
+    int ind = 0;
+    for(SET_ITR itr = v.begin();itr!=v.end();itr++)
     {
-        print_set(out,itr->first);
-        if(itr->second>=MIN_SUPPORT_PART)
-        rtn1[itr->first] = itr->second;
-        out<<" = "<<itr->second<<endl;
+        rtn[ind++].push_back(*itr);
     }
-    return rtn1;
+    return rtn;
 }
 
 /**
-utility function for joining and pruning item sets
+Function for finding the number of occurrences of each item vector
 **/
-bool is_feasible(set<int> s1,set<int> s2,set<int> &s,map<set<int>,int> m,int k)
-{
-    SET_ITR i = s1.begin(),j = s2.begin();
-    for(int l=0;l<k-1;l++)
-    {
-        if(*i!=*j)
-            return false;
-        s.insert(*i);
-        i++;j++;
-    }
-    if(*i>=*j)
-        return false;
-    s.insert(*i);
-    s.insert(*j);
-    i = s.begin();
-    set<int> h;
-    h = s;
-    while(i!=s.end())
-    {
-        h.erase(*i);
-        if(m.find(h)==m.end())
-            return false;
-        h.insert(*i);
-        i++;
-    }
 
-    return true;
-}
-
-/**
-Function for finding the number of occurrences of each item set
-**/
-void check_support_count(map<set<int>,int> &rtn,ofstream &out)
+map<vector<int>,int> check_support_count(vector<vector<int> > rtn,ofstream &out)
 {
     int t;
-    MAP_ITR itr1;
-    SET_ITR itr2;
+    int itr1,itr2,itr4;
     MAP_ITR2 itr3;
-    SET_ITR itr4;
     itr3 = tran.begin();
-    while(itr3!=tran.end())
+    map<vector<int>,int> rtn1;
+    for(itr1 = 0;itr1<rtn.size();itr1++)
     {
-        itr1 = rtn.begin();
-        while(itr1!=rtn.end())
+        itr3 = tran.begin();
+        t = 0;
+        while(itr3!=tran.end())
         {
-            itr2 = (itr1->first).begin();
-            itr4 = (itr3->second).begin();
-            while(itr2!=(itr1->first).end()&&itr4!=(itr3->second).end())
+            itr4 = 0;
+            itr2 = 0;
+            while(itr2!=(rtn[itr1]).size()&&itr4!=(itr3->second).size())
             {
-                if(*itr2==*itr4)
-                    {
-                        itr2++;itr4++;
-                    }
-                else if(*itr4<*itr2)
+                if(rtn[itr1][itr2]==(itr3->second)[itr4])
+                {
+                    itr2++;itr4++;
+                }
+                else if(rtn[itr1][itr2]>(itr3->second)[itr4])
                 {
                     itr4++;
                 }
                 else
                     break;
             }
-            if(itr2==(itr1->first).end())
+            if(itr2==(rtn[itr1]).size())
             {
-                rtn[itr1->first]++;
+                t++;
             }
-            itr1++;
+            itr3++;
         }
-        itr3++;
-    }
-    map<set<int>,int> rtn1;
-    itr1 = rtn.begin();
-    while(itr1!=rtn.end())
-    {
-        print_set(out,itr1->first);
-        out<<" = "<<itr1->second<<endl;
-        if(itr1->second>=MIN_SUPPORT_PART)
+        if(t>=MIN_SUPPORT_PART)
         {
-            rtn1[itr1->first] = itr1->second;
+            rtn1[rtn[itr1]] = t;
         }
-        itr1++;
+        print_vector(out,rtn[itr1]);
+        out<<" = "<<t<<endl;
     }
-    rtn = rtn1;
+   return rtn1;
 }
 
 /**
 utility function for joining Lk with itself to make ck and then L(k+1)
 **/
 
-map<set<int>,int> join(map<set<int>,int> lk,int k,ofstream &out)
+map<vector<int>,int> join(map<vector<int>,int> lk,int k,ofstream &out)
 {
-    map<set<int>,int> rtn;
-    set<int> s;
+    vector<vector<int> > rtn;
+    vector<int> s,h;
+    vector<int>::iterator i,j;
     MAP_ITR itr1,itr2;
+    int ind1,ind2;bool f = true;
     for(itr1 = lk.begin();itr1!=lk.end();itr1++)
     {
+   // cout<<"= ";
         itr2 = itr1;
         itr2++;
+
         while(itr2!=lk.end())
         {
-            if(is_feasible(itr1->first,itr2->first,s,lk,k))
+           f = true;
+            for(ind1=0,ind2=0;ind1<(itr1->first).size()-1,ind2<(itr2->first).size()-1;ind1++,ind2++)
             {
-                rtn[s] = 0;
+                if((itr1->first)[ind1]!=(itr2->first)[ind2])
+                {
+                    f = false;
+                    break;
+                }
+                s.push_back((itr1->first)[ind1]);
+            }
+            if(f)
+            {
+                s.push_back((itr1->first)[ind1]);
+                s.push_back((itr2->first)[ind2]);
+                i = s.begin();
+                int sto;
+                h = s;
+                j = h.begin();
+                while(i!=s.end())
+                {
+                    sto = *i;
+                    j = h.erase(j);
+                    if(lk.find(h)==lk.end())
+                    {
+                        f = false;
+                        break;
+                    }
+                    j = h.insert(j,sto);
+                    i++;j++;
+                }
+                if(f)
+                rtn.push_back(s);
             }
             s.clear();
             itr2++;
         }
     }
-    check_support_count(rtn,out);
-    return rtn;
+    return check_support_count(rtn,out);
 }
+
+
 /**
-Utility function for printing a set
+Utility function for printing a vector
 **/
-void print_set(ostream &out,set<int> s)
+
+
+void print_vector(ostream &out,vector<int> s)
 {
-    for(SET_ITR itr = s.begin();itr!=s.end();itr++)
+    for(vector_ITR itr = s.begin();itr!=s.end();itr++)
     {
         out<<*itr<<" ";
     }
@@ -193,66 +190,48 @@ void print_set(ostream &out,set<int> s)
 /**
 Driver program
 **/
-map<set<int>,int> finalise()
-{
-    map<set<int>,int> rtn;
-    for(MAP_ITR itr = final_set.begin();itr!=final_set.end();itr++)
-    {
-        if(itr->second>=MIN_SUPPORT)
-        rtn[itr->first] = itr->second;
-    }
-    return rtn;
-}
 int main()
 {
-    map<set<int>,int> rtn,prev;
+    vector<vector<int> > c;
+    map<vector<int>,int> rtn,prev;
     ofstream out;
     init();
-    out.open("output1.txt");
+    out.open("out.txt");
     f = true;
     int track = 1;
     out<<"*********************   for "<<track<<"th part   ************************"<<endl;
     while(f)
     {
-        rtn = read_set(out);
+        c = read_vector(out);
+        rtn = check_support_count(c,out);
         for(int k=1;rtn.size()!=0;k++)
         {
             out<<"L"<<k<<" :"<<endl;
-            cout<<"L"<<k<<" : "<<rtn.size()<<endl;
             for(MAP_ITR itr = rtn.begin();itr!=rtn.end();itr++)
             {
-                print_set(out,itr->first);
+                print_vector(out,itr->first);
                 out<<" = "<<itr->second<<endl;
-                if(final_set.find(itr->first)==final_set.end())
-                final_set[itr->first]=itr->second;
-                else
-                final_set[itr->first]+=itr->second;
+                final_vector[itr->first]+=itr->second;
             }
-            prev = rtn;
             out<<"C"<<k+1<<" : "<<endl;
             rtn = join(rtn,k,out);
-            if(rtn.empty())
-                out<<"*********************   for "<<track<<"th part   ************************"<<endl;
-        }
-        for(MAP_ITR itr = prev.begin();itr!=prev.end();itr++)
-        {
-            print_set(out,itr->first);
-            out<<" = "<<itr->second<<endl;
+            if(rtn.empty()&&f)
+                out<<"*********************   for "<<track+1<<"th part   ************************"<<endl;
         }
         tran.clear();
         track++;
     }
-    final_set = finalise();
     out<<"******* final results *******"<<endl;
-    for(MAP_ITR itr = final_set.begin();itr!=final_set.end();itr++)
+    for(MAP_ITR itr = final_vector.begin();itr!=final_vector.end();itr++)
         {
-            print_set(out,itr->first);
-            print_set(cout,itr->first);
-            out<<" = "<<itr->second<<endl;
-            cout<<" = "<<itr->second<<endl;
+            if(itr->second>=MIN_SUPPORT)
+            {
+                print_vector(out,itr->first);
+                out<<" = "<<itr->second<<endl;
+            }
         }
     out.close();
-    cout<<"size = "<<final_set.size()<<endl;
+    cout<<"size = "<<final_vector.size()<<endl;
     return 0;
 }
 
